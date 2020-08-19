a4ea0774b56ebf357edc159d4f5c84b0aab7f541
MirAtharHussain <miratharhussain52@gmail.com>
2020-08-19 16:55:32 +0530

    [Hussain]ADD:Corrected the summation

--- Fibonacci1.pl
+++ Fibonacci1.pl
@@ -4,11 +4,11 @@
 
 
 	
-my $n=20;
-my $sum=0;
+my $n=5;
 my $i=0;
 my $a=0;
 my $b=1;
+my $sum=$a+$b;
 my $temp=0;
 my $count=0;
 
@@ -29,9 +29,9 @@ $sum = $sum + $temp;
 }
 $i++;
 $count++;
-print("$temp");
+print("$temp"," ");
 }
 	
-print("\tthe sum of fibonacci numbers is\t : $sum");
-print("\t the no of fibonacci numbers are: $count.\n");
+print("The sum of fibonacci numbers is: $sum","\n");
+print("The no of fibonacci numbers are: $count","\n");
 
